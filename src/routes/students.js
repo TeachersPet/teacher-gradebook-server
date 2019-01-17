079d1b0aaaeed2133a4419077aa02abb49519855
Celia Marshall <celia.m@gmail.com>
2019-01-17 11:32:27 -0800

    add auth functions in front of routes

--- src/routes/students.js
+++ src/routes/students.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router({mergeParams: true});
 const studentController = require('../controllers/students')
-const authController = require('../controllers/auth')
 
 router.get('/', studentController.getStudents)
 
--- src/routes/students_assignments.js
+++ src/routes/students_assignments.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router({mergeParams: true});
 const studentAssignmentController = require('../controllers/students_assignments')
-const authController = require('../controllers/auth')
 
 router.put('/:studentId', studentAssignmentController.updateAssignment)
 
